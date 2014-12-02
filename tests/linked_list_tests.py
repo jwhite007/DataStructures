918c017340e4fa42d1ded5f92965001f403c28a8
James White <jwhite007@comcast.net>
2014-12-02 11:54:05 -0800

    linked_list_tests.py updated to use return_kth_from_nth() instead of print_kth_from_nth()

--- tests/linked_list_tests.py
+++ tests/linked_list_tests.py
@@ -60,17 +60,17 @@ class TestLinky(unittest.TestCase):
         self.linky = LinkedList()
         self.assertListEqual([], self.linky.print_me())
 
-    def test_print_0th_from_4th(self):
-        self.assertEqual(1, self.linky.print_kth_from_nth(0, 4))
+    def test_return_0th_from_4th(self):
+        self.assertEqual(1, self.linky.return_kth_from_nth(0, 4))
 
-    def test_print_1st_from_4th(self):
-        self.assertEqual(2, self.linky.print_kth_from_nth(1, 4))
+    def test_return_1st_from_4th(self):
+        self.assertEqual(2, self.linky.return_kth_from_nth(1, 4))
 
-    def test_print_0th_from_2nd(self):
-        self.assertEqual(3, self.linky.print_kth_from_nth(0, 2))
+    def test_return_0th_from_2nd(self):
+        self.assertEqual(3, self.linky.return_kth_from_nth(0, 2))
 
-    def test_print_1st_from_2nd(self):
-        self.assertEqual(4, self.linky.print_kth_from_nth(1, 2))
+    def test_return_1st_from_2nd(self):
+        self.assertEqual(4, self.linky.return_kth_from_nth(1, 2))
 
     def test_partition_around_x(self):
         self.linky = LinkedList()
